96625d6a4948c5b508f46b0d78fb95e97f6e77a5
LazySelkie <54956262+LazySelkie@users.noreply.github.com>
2023-06-05 13:33:18 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Hollow Knight RPG ru'
+copyright = '2021, Team Cherry'
+author = 'Please see main page'
 
 release = '0.1'
 version = '0.1.0'
